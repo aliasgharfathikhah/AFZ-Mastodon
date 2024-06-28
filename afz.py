68caf42c43fd0237390b1e1ebc0f611c6a14531d
ali fathi <ali.fathi.13121110@gmail.com>
2024-06-28 10:53:26 +0330

    fix: fix bug

--- afz.py
+++ afz.py
@@ -1,3 +1,4 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -9,7 +10,6 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env('.env')
-
 
 def get_wikipedia_summary(search_text, lang='en'):
     try:
@@ -49,12 +49,69 @@ def get_wikipedia_summary(search_text, lang='en'):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
-    
-    
+
+def run_bot(driver):
+    driver.implicitly_wait(10)
+    driver.get('https://mastodon.world/notifications')
+    driver.implicitly_wait(10)
+    driver.find_element(By.CLASS_NAME, 'active').click()
+    try:
+        while True:
+            user_name = ''
+            driver.implicitly_wait(10)
+            messages = driver.find_elements(By.CSS_SELECTOR, 'div.status')
+            for message in messages:
+                    driver.implicitly_wait(10)
+                    star_button = message.find_element(By.CSS_SELECTOR, 'button.star-icon')
+                    replay_button = message.find_element(By.CSS_SELECTOR, 'button.icon-button--with-counter')
+        
+                    if "active" not in star_button.get_attribute('class'):
+                        text_element = message.find_element(By.CSS_SELECTOR, 'div.status__content__text')
+                        user_element = message.find_element(By.CLASS_NAME, 'display-name__account')
+                        user_name = user_element.text
+                        print(f"Message: {text_element.text} - {user_element.text}")
+                        text = text_element.text.replace('@afz', '').replace('\n', ' ').replace('@afz\n', '').replace('dictator', '').replace('Is', '').strip()
+                        print(text)
+                        language = detect(text)
+                        lines = get_wikipedia_summary(text, language)
+                        print(lines)
+                        if lines:
+                            driver.implicitly_wait(10)
+                            star_button.click()
+                            driver.implicitly_wait(10)
+                            replay_button.click()
+                            driver.find_element(By.CLASS_NAME, 'autosuggest-textarea__textarea').send_keys(f' (¬‿¬) {". ".join(lines)}')
+                            driver.implicitly_wait(10)
+                            driver.find_element(By.CLASS_NAME, 'compose-form__publish-button-wrapper').click()
+                        else:
+                            driver.implicitly_wait(10)
+                            star_button.click()
+                            driver.implicitly_wait(10)
+                            replay_button.click()
+                            driver.find_element(By.CLASS_NAME, 'autosuggest-textarea__textarea').send_keys("╰（‵□′）╯ No data found, please ask more precisely! Or, if the data is available on Wikipedia, raise the issue with our support. @FATHI@mstdn.social Thanks")
+                            driver.implicitly_wait(10)
+                            driver.find_element(By.CLASS_NAME, 'compose-form__publish-button-wrapper').click()
+                        print("------------------------------------------")
+    except selenium.common.exceptions.WebDriverException:
+        print("Session timed out. Restarting...")
+        driver.quit()
+        driver = webdriver.Remote(
+            command_executor=env.str('SERVER_URL') + 'webdriver',
+            options=chrome_options,
+        )
+        driver.get(url)
+        
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        
+        run_bot(driver)
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.set_capability('browserless:token', env.str('TOKEN'))
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
 
 driver = webdriver.Remote(
     command_executor=env.str('SERVER_URL') + 'webdriver',
@@ -74,45 +131,6 @@ driver.find_element(By.ID, 'user_password').send_keys(env.str('USER_PASSWORD'))
 
 driver.find_element(By.CLASS_NAME, 'btn').click()
 
-driver.implicitly_wait(10)
-driver.get('https://mastodon.world/notifications')
-driver.implicitly_wait(10)
-driver.find_element(By.CLASS_NAME, 'active').click()
+cookies = driver.get_cookies()
 
-while True:
-    driver.implicitly_wait(10)
-    messages = driver.find_elements(By.CSS_SELECTOR, 'div.status')
-    user_name = ''
-    driver.implicitly_wait(10)
-    messages = driver.find_elements(By.CSS_SELECTOR, 'div.status')
-    for message in messages:
-        try:
-            driver.implicitly_wait(10)
-            star_button = message.find_element(By.CSS_SELECTOR, 'button.star-icon')
-            replay_button = message.find_element(By.CSS_SELECTOR, 'button.icon-button--with-counter')
-            
-            if "active" not in star_button.get_attribute('class'):
-                text_element = message.find_element(By.CSS_SELECTOR, 'div.status__content__text')
-                user_element = message.find_element(By.CLASS_NAME, 'display-name__account')
-                user_name = user_element.text
-                print(f"Message: {text_element.text} - {user_element.text}")
-                text = text_element.text.replace('@afz', '').replace('\n', ' ').replace('@afz\n', '').replace('dictator', '').replace('Is', '').strip()
-                print(text)
-                language = detect(text)
-                lines = get_wikipedia_summary(text, language)
-                print(lines)
-                print("------------------------------------------")
-                if lines:
-                    star_button.click()
-                    replay_button.click()
-                    driver.find_element(By.CLASS_NAME, 'autosuggest-textarea__textarea').send_keys(f' {". ".join(lines)}')
-                    driver.find_element(By.CLASS_NAME, 'compose-form__publish-button-wrapper').click()
-                else:
-                    star_button.click()
-                    replay_button.click()
-                    driver.find_element(By.CLASS_NAME, 'autosuggest-textarea__textarea').send_keys("""No data found, please ask more precisely!
-                    Or, if the data is available on Wikipedia, raise the issue with our support. @FATHI@mstdn.social
-                    Thanks""")
-                    driver.find_element(By.CLASS_NAME, 'compose-form__publish-button-wrapper').click()
-        except:
-            continue
+run_bot(driver)
